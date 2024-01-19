0736480efad3d47b0af688715db29cb8ae2e652d
ismailouzy <ismailouzal@gmail.com>
2024-01-18 18:00:02 -0800

    swap

--- monty.h
+++ monty.h
@@ -25,5 +25,8 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
 void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+
+
 #endif
 
