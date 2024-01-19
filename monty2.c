96cfd4d5c4884ac8f95545f30c561ca8be8692e4
ismailouzy <ismailouzal@gmail.com>
2024-01-18 18:40:49 -0800

    div

--- monty2.c
+++ monty2.c
@@ -61,3 +61,25 @@ void sub(stack_t **stack, unsigned int line_number)
 	(*stack)->next->n -= (*stack)->n;
 	pop(stack, line_number);
 }
+/**
+ * div_op - entry point
+ * @stack: stack
+ * @line_number: counter
+ */
+void div_op(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n /= (*stack)->n;
+	pop(stack, line_number);
+}
