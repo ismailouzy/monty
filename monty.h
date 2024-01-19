6b8dacc6dc22958e87675d6ce6264802bdc3e306
ismailouzy <ismailouzal@gmail.com>
2024-01-18 18:03:31 -0800

    add

--- monty.h
+++ monty.h
@@ -26,7 +26,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void free_stack(stack_t **stack);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
-
+void add(stack_t **stack, unsigned int line_number);
 
 #endif
 
--- monty2.c
+++ monty2.c
@@ -18,3 +18,19 @@ void swap(stack_t **stack, unsigned int line_number)
 	(*stack)->n = (*stack)->next->n;
 	(*stack)->next->n = temp;
 }
+/**
+ * add - entry point
+ * @stack: stack
+ * @line_number: counter
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n += (*stack)->n;
+	pop(stack, line_number);
+}
