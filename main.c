64f1ee04053cc01e8f3643ce1b7cb36d517bb879
ismailouzy <ismailouzal@gmail.com>
2024-01-18 18:36:08 -0800

    sub

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL;
 	instruction_t instructions[] = {{"push", push}, {"pall", pall}, 
 		{"pint", pint}, {"pop", pop}, {"swap", swap}, {"add", add},
-		{"nop", nop}};
+		{"nop", nop}, {"sub", sub}};
 	char opcode[100], *fmt = "L%d: %s %s\n";
 	unsigned int line_number = 0, i;
 
--- monty2.c
+++ monty2.c
@@ -45,3 +45,19 @@ void nop(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 
 }
+/**
+ * sub - entry point
+ * @stack: stack
+ * @line_number: counter
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n -= (*stack)->n;
+	pop(stack, line_number);
+}
